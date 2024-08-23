fee037e835835c8858c8859a2a926ac166d0e4fa
Lucas Barboza <Lucasrb18@gmail.com>
2024-08-22 21:29:29 -0300

    Backend Finished

--- src/server/api/routers/todo.ts
+++ src/server/api/routers/todo.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { todoInput } from "~/types";
 
 import {
   createTRPCRouter,
@@ -8,8 +9,15 @@ import {
 
 export const todoRouter = createTRPCRouter({
 
-  all: protectedProcedure.query(() => {
-    
+  all: protectedProcedure.query(async ({ctx}) => {
+    const todos = await ctx.db.todo.findMany({
+      where:{
+        userId: ctx.session.user.id
+      },
+    });
+
+    console.log(todos.map(({id, text, done}) => ({id, text, done})))
+
     return [{
       id: 'fake1',
       text: 'fake text',
@@ -21,5 +29,40 @@ export const todoRouter = createTRPCRouter({
       done: true
     }]
   }),
+
+  create: protectedProcedure.input(todoInput).mutation(async ({ ctx, input }) => {
+    return ctx.db.todo.create({
+      data:{
+        text:input,
+        user:{
+          connect:{
+            id: ctx.session.user.id
+          }
+        }
+      }
+    })
+  }),
+
+  delete: protectedProcedure.input(z.string()).mutation(async ({ ctx, input }) => {
+    return ctx.db.todo.delete({
+      where:{
+        id: input
+      }
+    })
+  }),
+
+  toggle: protectedProcedure.input(z.object({
+    id: z.string(),
+    done: z.boolean()
+  })).mutation(async ({ ctx, input: {id, done} }) => {
+    return ctx.db.todo.update({
+      where:{
+        id
+      },
+      data:{
+        done
+      }
+    })
+  })
 
 });
